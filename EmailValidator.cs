21c12c9bb682a925ed26f87bfd6578c21980a81a
Devin Woods <Woods.devinj@gmail.com>
2017-08-11 11:26:15 -0700

    Create EmailValidator.cs

---
+++ EmailValidator.cs
@@ -0,0 +1,34 @@
+public class EmailValidator
+    {
+        public static void Main(string[] args)
+        {
+            // Sample data array
+            string[] emails = {"d@gmail.com", 
+                               "x@yahoo.com" , 
+                               "y@topsecret.gov" ,
+                               "xyz@yahoo" , 
+                               "@gmail.com" ,
+                               "xxxxxgmail.com"};
+            
+            // Loops to go through array and validate data          
+            for(int i = 0; i < emails.Length;i++)
+            {
+                   EmailValidator(emails[i]);
+            }
+        }
+        
+        public static void EmailValidator(string email)
+        {
+            string PATTERN = @"\S{1,}" + // Minimum of 1 non-whitespace character
+                             "@" + // @ symbol
+                             @"\S{1,}" + // Minimum of 1 non-whitespace character
+                             @"(\.com|\.gov|\.edu|\.co|\.org){1,}$"; // Tail
+            
+            // New Regex object constructed with the pattern
+            Regex regex = new Regex(PATTERN);
+            
+            // Print whether it is valid or not using ternary statement
+            Console.WriteLine(regex.IsMatch(email) ? email + " is a valid email." : email + " is NOT a valid email.");
+        }
+        
+    }
